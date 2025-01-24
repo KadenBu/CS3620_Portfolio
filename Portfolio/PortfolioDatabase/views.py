a11093ec4537185213bfa7fbc5c9d66fd0520a79
KadenBu <kadenbuchanan@mail.weber.edu>
2025-01-24 01:20:12 -0700

    View Changes

--- Portfolio/PortfolioDatabase/views.py
+++ Portfolio/PortfolioDatabase/views.py
@@ -7,12 +7,22 @@ def home(request):
     return HttpResponse("Hi I'm Kaden, welcome to my portfolio! I like to program and play basketball. Recently I've gotten into snowboarding.")
 
 def hobbies(request):
-    hobby_list = Hobby.objects.all()
-    return HttpResponse(hobby_list)
+    hobby_list = Hobby.objects.all().values()
+    output = ""
+    for i in hobby_list:
+        output = output + i['name'] + ": "
+        output = output + i['desc'] + "<br>"
+        output = output + "<br>"
+    return HttpResponse(output)
 
 def portfolio(request):
-    project_list = Project.objects.all()
-    return HttpResponse(project_list)
+    project_list = Project.objects.all().values()
+    output = ""
+    for i in project_list:
+        output = output + i['name'] + ": "
+        output = output + i['desc'] + "<br>"
+        output = output + "<br>"
+    return HttpResponse(output)
 
 def contact(request):
     return HttpResponse("kadenbuchanan@mail.weber.edu")
